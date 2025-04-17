eeac45b75b50a939b123b5717fe47514aa8ee5ad
LeBaoKhanh512010 <2106084@lsts.edu.vn>
2025-04-17 13:06:39 +0000

    Update the content

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/wordle_game", methods=["GET", "POST"])
+def wordle_game():
+    return render_template("wordle_game.html")
+
 if __name__ == "__main__":
     app.run()
